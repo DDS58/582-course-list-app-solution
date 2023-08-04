8a01579c67c42fb7fc4c2814913bc6eb1052a88b
David <2295458@edu.vaniercollege.qc.ca>
2023-08-04 16:07:47 -0400

    emptied input text

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
   name: "App",
   data() {
     return {
-      inputText: "asdasd",
+      inputText: "",
       coursesSelected: 0,
       coursesSelectedList: [],
       courses: [
